6c902b52bc4e50c560fe329c3a0fe43fe7d58a66
pagpond005 <pagpond133@hotmail.com>
2022-12-07 21:52:07 +0700

    [UPDATE] - clean routing folder and files

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,9 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import FolderList from '../screens/FolderList';
-import TemplatesList from '../screens/TemplatesList';
-import TemplatesDetails from '../screens/TemplatesDetails';
+import FolderList from '../screens/folderScreen';
+import TemplatesList from '../screens/templatesListScreen';
+import TemplatesDetails from '../screens/templatesDetailsScreen';
 
 const Stack = createNativeStackNavigator();
 
--- src/screens/folderScreen/index.js
+++ src/screens/folderScreen/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Text } from 'react-native';
 
 const FolderList = () => {
-    const data = require('../resorce/constant/templates.json');
-
     return (
         <Text>Hello, I am your cat!</Text>
     );
